cf40850e2894df60fb6dce77bd3860898a0f4949
irina777-create <grigoreva108@gmail.com>
2025-03-25 20:20:57 +0200

    lesson -18

---
+++ cypress/header.spec.js
@@ -0,0 +1,28 @@
+
+describe('Перевірка кнопок ', () => {
+        beforeEach(() => {
+
+            it('Basic HOMEWORK login', () => {
+                cy.visit('https://qauto.forstudy.space/', {
+                    auth: {
+                        username: 'guest',
+                        password: 'welcome2qauto'
+                    }
+                })
+            })
+        it('Перевіряє, що кнопки соцмереж у футері існують і мають правильні посилання', () => {
+            cy.get('.socials_link a[href="https://www.facebook.com"]').should('be.visible');
+            cy.get('.socials_link a[href="https://t.me/"]').should('be.visible');
+            cy.get('.socials_link a[href="https://www.youtube.com"]').should('be.visible');
+            cy.get('.socials_link a[href="https://www.instagram.com"]').should('be.visible');
+            cy.get('.socials_link a[href="https://www.linkedin.com"]').should('be.visible');
+            cy.get('.socials_link a[href="mailto:support@ithillel.ua"]').should('be.visible');
+            cy.get('.contacts_link a[href="https://ithillel.ua"]').should('be.visible');
+        });
+
+        it('Перевіряє кнопку Sign up', () => {
+            cy.get('.section.hero a').should('be.visible').and('contain.text', 'Sign up');
+        });
+    });
+
+});
